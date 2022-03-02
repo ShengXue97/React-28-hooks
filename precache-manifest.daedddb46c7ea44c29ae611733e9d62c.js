adcd4dc07609f080d259b1dca3a168697d678bfc
unknown <simshengxue1997@gmail.com>
2022-03-03 01:50:38 +0800

    Updates

---
+++ precache-manifest.daedddb46c7ea44c29ae611733e9d62c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0351b85ca51052c06a3960797635322",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c6f8f714eba6c6b7d9c",
+    "url": "/static/css/main.2ff2c3d5.chunk.css"
+  },
+  {
+    "revision": "71be980611f49b0fe1f8",
+    "url": "/static/js/2.d612d8b9.chunk.js"
+  },
+  {
+    "revision": "8c6f8f714eba6c6b7d9c",
+    "url": "/static/js/main.83f2e634.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
